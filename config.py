620f6f04cf3e1143d4db2d587b0b7af15a337645
mo-hunter123 <mohamed.makromi.2000@gmail.com>
2020-06-18 00:48:30 +0100

    first

---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'jkjkjskjskjkg32k34k32kjk32j4j234kl2j3'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
